3ab3fe0a70f34b5174c13271f0cf24b7ea961c29
Amine-Ayoub Bigham <aminebigham@hotmail.com>
2021-03-30 15:24:45 +0200

    Add Ride class

---
+++ src/main/java/atl/stibride/model/Ride.java
@@ -0,0 +1,21 @@
+package atl.stibride.model;
+
+import atl.stibride.dto.StationDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ride {
+
+    /**
+     * Computes the best path to take between the two stations.
+     *
+     * @param start the starting station.
+     * @param end   the end station.
+     * @return a list of stations describing the best path to take between the two stations.
+     */
+    public static List<StationDto> getRide(StationDto start, StationDto end) {
+        // TODO
+        return new ArrayList<>();
+    }
+}
